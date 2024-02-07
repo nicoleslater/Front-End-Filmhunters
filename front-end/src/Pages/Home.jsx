b16328c2bb9761de011c2e94f398024a1aaf6951
Nicole Slater <nicoleslater@pursuit.org>
2024-02-07 14:30:43 -0500

    Updated Show Page and Home Page

--- front-end/src/Pages/Home.jsx
+++ front-end/src/Pages/Home.jsx
@@ -1,10 +1,15 @@
-
+import React from 'react';
+import { Link } from 'react-router-dom'; 
 
 const Home = () => {
     return (
-        <div>
-            <h1>FILMHUNTERS!</h1>
-            <h1>Let's get Diggin'</h1>
+        <div className="home-container">
+            <h1>Welcome to FILMHUNTERS!</h1>
+            <p>Explore the world of cinema like never before.</p>
+            <div className="cta-buttons">
+                <Link to="/movies" className="btn btn-primary">Browse Movies</Link>
+                <Link to="/favorites" className="btn btn-secondary">Explore Favorites</Link>
+            </div>
         </div>
     );
 };
--- front-end/src/Pages/Show.jsx
+++ front-end/src/Pages/Show.jsx
@@ -1,10 +1,10 @@
-import MovieDetails from "../Components/MovieDetails";
+import Movie from "../Components/Movie";
 
 function Show(){
     return (
         <div className="Show">
           <h2>Show</h2>
-          <MovieDetails />
+          <Movie />
         </div>
       );
 }
