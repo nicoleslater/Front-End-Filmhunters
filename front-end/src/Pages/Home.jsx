48213c7bb5ca9564bbca03664741fffad6e4aad2
Nicole Slater <nicoleslater@pursuit.org>
2024-02-08 18:08:34 -0500

    Updated Home Page

--- front-end/src/Pages/Home.jsx
+++ front-end/src/Pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 const Home = () => {
     return (
-        <div>
-                <h2>Welcome to FilmHunters</h2>
-            <p>Discover and explore your favorite movies!</p>
+        <div className="home-container">
+            <h2 className="home-heading">Welcome to FilmHunters</h2>
+            <p className="home-text">Discover and explore your favorite movies!</p>
             <div>
-                <Link to="/movies">Explore Movies</Link>
+                <Link to="/movies" className="explore-link">Explore Movies</Link>
             </div>
         </div>
     );
