c9f28b0652bd3f488c26c6c48be4184bbf0a2878
Nicole Slater <nicoleslater@pursuit.org>
2024-02-08 09:27:04 -0500

    Updated Movie Component

--- front-end/src/Components/Movie.jsx
+++ front-end/src/Components/Movie.jsx
@@ -3,29 +3,29 @@ import React from "react";
 
 
 
-function Movie({ movie }){
+function Movie({ movie }) {
 
 
- 
-  return(
+
+  return (
     <div>
-     <div className="card w-96 bg-primary text-primary-content">
-  <div className="card-body">
-    <h2 className="card-title">{movie.title}</h2>
+      <div className="card w-96 bg-primary text-primary-content">
+        <div className="card-body">
+          <h2 className="card-title">{movie.title}</h2>
           <p>{movie.released}</p>
           <p>{movie.director}</p>
           <p>{movie.genre}</p>
           <p>{movie.rated}</p>
           <p>{movie.dvd}</p>
           <p>{movie.imdb_Ratings}</p>
-    <div className="card-actions justify-end">
-    <div>
-          <Link to={`/movies/${movie.id}`}>🍿</Link>
+          <div className="card-actions justify-end">
+            <div>
+              <Link to={`/movies/${movie.id}`}>🍿</Link>
+            </div>
           </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-         </div>
   )
 }
 
--- front-end/src/Components/Movies.jsx
+++ front-end/src/Components/Movies.jsx
@@ -1,34 +1,36 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Movie from "./Movie";
 
 const API = import.meta.env.VITE_API_URL;
 
 function Movies() {
-  const [ movies, setMovies ] = useState([]);
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-fetch(`${API}/movies`)
-.then(response => response.json())
-.then((responseJSON) => {
-  console.log(responseJSON);
-  setMovies(responseJSON.data.payload);
-})
-.catch((error) => {
-  console.log(error);
-})
-}, []);
+    fetch(`${API}/movies`)
+      .then((response) => response.json())
+      .then((responseJSON) => {
+        console.log(responseJSON);
+        setMovies(responseJSON.data.payload);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
+    <div> 
     <div className="Movies">
       <section>
-        <table>
-            <tbody>
-            {movies.map((movie) => {
-              return <Movie key={movie.id} movie={movie} />;
-            })}
-          </tbody>
-        </table>
+      
+        <div>
+            {movies.map((movie) => (
+              <Movie key={movie.id} movie={movie} />
+            ))}
+         </div>
+
       </section>
+    </div>
     </div>
   );
 }
