d5d9235405d87c5a4e8fb41ccea8db08fe222b42
Nicole Slater <nicoleslater@pursuit.org>
2024-02-08 19:10:21 -0500

    Added picture to Home

--- front-end/src/Pages/Home.jsx
+++ front-end/src/Pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Poster from "../assets/Poster.jpeg"
+import "./Home.css";
 
 
 const Home = () => {
     return (
         <div className="home-container">
+            <img src={Poster} alt="Poster" className="poster"></img>
             <h2 className="home-heading">Welcome to FilmHunters</h2>
             <p className="home-text">Discover and explore your favorite movies!</p>
             <div>
